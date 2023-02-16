0b12a86c6921c867caac05031b10f38e18bc4726
felipe-cruz <piradous@gmail.com>
2023-02-16 14:46:15 -0300

    primeiro commit

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,21 +1,14 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
+    with open(path) as file:
+        file_reader = csv.reader(file)
+    header, *data = file_reader
+    return {header: data}
     raise NotImplementedError
 
 
