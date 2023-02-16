2603af49857a5cf479c8f65f8b0430278a2e5ef2
felipe-cruz <piradous@gmail.com>
2023-02-16 16:47:11 -0300

    feat: function get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,15 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    jobs = read(path)
+    unique_industries = []
+    for job in jobs:
+        if job['industry'] not in unique_industries:
+            unique_industries.append(job['industry'])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return unique_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
