64c7c9ca5b8ce16e1fcd2917b82451c4893dd2fc
felipe-cruz <piradous@gmail.com>
2023-02-20 15:31:34 -0300

    feat: test function count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+import pytest
 
 
 def test_counter():
-    pass
+    assert count_ocurrences('data/jobs.csv', 'systems') == 3176
+    assert count_ocurrences('data/jobs.csv', 'Marketing') == 1259
+
+    with pytest.raises(AttributeError):
+        count_ocurrences('data/jobs.csv', 156156)
