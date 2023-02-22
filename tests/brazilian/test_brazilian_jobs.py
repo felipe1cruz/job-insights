5a685670d62deaddb255312573ab1dbea4e8e481
felipe-cruz <piradous@gmail.com>
2023-02-22 10:11:00 -0300

    fix: function test read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,14 +1,10 @@
 from src.pre_built.brazilian_jobs import read_brazilian_file
 
-result_index_zero = {'title': 'Maquinista',
-                     'salary': '2000',
-                     'type': 'trainee'}
 path = 'tests/mocks/brazilians_jobs.csv'
 
 
 def test_brazilian_jobs():
     brazilian_jobs = read_brazilian_file(path)
-    assert brazilian_jobs[0] == result_index_zero
     for job in brazilian_jobs:
-        keys = [job.keys()]
+        keys = list(job.keys())
         assert keys == ['title', 'salary', 'type']
